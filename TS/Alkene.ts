74547004bf5a3b0a5760411fe40192e76e463680
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-09-30 11:30:37 +0100

    Update Alkene.ts

--- TS/Alkene.ts
+++ TS/Alkene.ts
@@ -1,22 +1,22 @@
-interface Alkene {
+interface CH22 {
     carbonCount: number;
     hydrogenCount: number;
 }
-class C2H2 implements Alkene {
+class C2H2 implements CH22 {
     readonly carbonCount: number = 2
     readonly hydrogenCount: number = 2
 }
 
-class C4H2 implements Alkene {
+class C4H2 implements CH22 {
     readonly carbonCount: number = 4
     readonly hydrogenCount: number = 2
 }
 
-class C6H2 implements Alkene {
+class C6H2 implements CH22 {
     readonly carbonCount: number = 6
     readonly hydrogenCount: number = 2
 }
-void function *generateAlkynes(){
+void function *generateCH22(){
     yield new C2H2()
     yield new C4H2()
     yield new C6H2()
