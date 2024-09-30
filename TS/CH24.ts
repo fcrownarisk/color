3229e1d94c20e52eaf90a0c74622b5f246d53a56
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-09-30 11:38:29 +0100

    Update CH24.ts

--- TS/CH24.ts
+++ TS/CH24.ts
@@ -3,17 +3,16 @@ interface methines{
     hydrogenCount: number;
 }
 class C2H4 implements methines {
-    readonly carbonCount: number = 2;
-    readonly hydrogenCount: number = 4;
+     carbonCount: number = 2
+     hydrogenCount: number = 4
 }
 
 class C6H8 implements methines {
-    readonly carbonCount: number = 6;
-    readonly hydrogenCount: number = 8;
+     carbonCount: number = 6
+     hydrogenCount: number = 8
 }
 
 class C14H16 implements methines {
-    readonly carbonCount: number = 14;
-    readonly hydrogenCount: number = 16;
+     carbonCount: number = 14
+     hydrogenCount: number = 16
 }
-console.log('CH2','C6H8','C14H16')
