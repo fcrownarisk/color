61bf4a5067a2e64adef590560a0432e5021f9659
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-09-30 11:38:46 +0100

    Update CH22.ts

--- TS/CH22.ts
+++ TS/CH22.ts
@@ -3,18 +3,18 @@ interface CH22 {
     hydrogenCount: number;
 }
 class C2H2 implements CH22 {
-    readonly carbonCount: number = 2
-    readonly hydrogenCount: number = 2
+     carbonCount: number = 2
+     hydrogenCount: number = 2
 }
 
 class C4H2 implements CH22 {
-    readonly carbonCount: number = 4
-    readonly hydrogenCount: number = 2
+     carbonCount: number = 4
+     hydrogenCount: number = 2
 }
 
 class C6H2 implements CH22 {
-    readonly carbonCount: number = 6
-    readonly hydrogenCount: number = 2
+     carbonCount: number = 6
+     hydrogenCount: number = 2
 }
 void function *generateCH22(){
     yield new C2H2()
