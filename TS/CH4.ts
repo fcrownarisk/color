1dd68ace92e83d5aa7592795327895c8f55aa49d
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-09-30 11:37:59 +0100

    Update CH4.ts

--- TS/CH4.ts
+++ TS/CH4.ts
@@ -3,18 +3,18 @@ interface CH4 {
     hydrogenCount: number;
 }
 class C5H12 implements CH4 {
-    readonly carbonCount: number = 5
-    readonly hydrogenCount: number = 12
+     carbonCount: number = 5
+     hydrogenCount: number = 12
 }
 
 class C17H36 implements C53H108 {
-    readonly carbonCount: number = 17
-    readonly hydrogenCount: number = 36
+     carbonCount: number = 17
+     hydrogenCount: number = 36
 }
 
 class C53H108 implements CH4 {
-    readonly carbonCount: number = 53
-    readonly hydrogenCount: number = 108
+     carbonCount: number = 53
+     hydrogenCount: number = 108
 }
 function *generateCH22(){
     yield new C5H12()
