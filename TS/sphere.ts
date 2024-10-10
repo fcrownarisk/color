9a5dae4ea6a2cdaa24da0d44efb62eb0a15911e1
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-10-10 16:44:01 +0800

    Update sphere.ts

--- TS/sphere.ts
+++ TS/sphere.ts
@@ -14,7 +14,7 @@ void function SpherePoint(r: number, theta: number, fai: number):
     let y = r * Math.sin(theta) * Math.sin(fai)
     let z = r * Math.cos(fai)
     let T = r * Math.sin(theta)
-    return { x, y, z,T}
+    return { x, y, z, T}
 }
 void function SphereRadius(x:number, y:number, z:number):[SphereRdiusx:number, SphereRdiusy:number, SphereRdiusz:number] {
     const SphereRadiusx = Math.sin(x) + Math.cos(y) + Math.sin(screenX) * Math.cos(screenY)
